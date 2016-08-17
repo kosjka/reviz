97509ea81c06f8c81817ec9e6492d1c70cd49e0c
kosjka <kosjkawork@gmail.com>
2016-08-17 09:23:50 +0500

    Added database in Assets.\nAdded import the database from the file system.\nAdded database export via e-mail program.

---
+++ app/src/main/java/ru/kosjka/reviz/DBHelper.java
@@ -0,0 +1,83 @@
+package ru.kosjka.reviz;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+/**
+ * Created by Артём on 10.08.2016.
+ */
+//public class DBHelper extends SQLiteOpenHelper {
+//    private static final String NAME = "reviz.db";
+//    private static final int version = 1;
+//    public DBHelper(Context context) {
+//        super(context, NAME, null, version);
+//    }
+//    private final String MYLOG = "MainActivity";
+//    @Override
+//    public void onCreate(SQLiteDatabase db) {
+//        Log.d(MYLOG, "onCreate start");
+//        //db.execSQL("drop table if exits goods");
+//        //db.execSQL("drop table if exits barcodes");
+//        db.execSQL("create table goods ( ngoods integer primary key autoincrement, fullname text, qty integer default 0)");
+//        db.execSQL("create table barcodes (barcode text primary key, ngoods integer)");
+//
+////        db.execSQL("insert into goods (ngoods,fullname) values(10000,'Папка темно зеленая');");
+////        db.execSQL("insert into goods (ngoods,fullname) values(10001,'Bantex папка зеленая');");
+////        db.execSQL("insert into goods (ngoods,fullname) values(10002,'ErichKrause синий');");
+////        db.execSQL("insert into goods (ngoods,fullname) values(10003,'EXPERT complete серая');");
+//        db.execSQL("insert into goods (ngoods,fullname) values(10004,'EXPERT complete красная');");
+//
+////        db.execSQL("insert into barcodes (barcode,ngoods) values('46000025165',10000);");
+////        db.execSQL("insert into barcodes (barcode,ngoods) values('5702231446156',10001);");
+////        db.execSQL("insert into barcodes (barcode,ngoods) values('4601921006964',10002);");
+////        db.execSQL("insert into barcodes (barcode,ngoods) values('4603515000518',10003);");
+//        db.execSQL("insert into barcodes (barcode,ngoods) values('4600000251615',10004);");
+//        Log.d(MYLOG, "onCreate stop");
+//    }
+//
+//    @Override
+//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        db.execSQL("drop table if exists goods");
+//        db.execSQL("drop table if exists barcodes");
+//        onCreate(db);
+//    }
+//}
+public class DBHelper extends SQLiteOpenHelper {
+
+
+    public static final String DBNAME = "reviz.db";
+    public static final String DBLOCATION = "/data/data/ru.kosjka.reviz/databases/";
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    public DBHelper(Context context) {
+        super(context, DBNAME, null, 1);
+        this.mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void openDatabase() {
+        String dbPath = mContext.getDatabasePath(DBNAME).getPath();
+        if (mDatabase != null && mDatabase.isOpen()) {
+            return;
+        }
+        mDatabase = SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    public void closeDatabase() {
+        if (mDatabase != null) {
+            mDatabase.close();
+        }
+    }
+}
--- app/src/main/java/ru/kosjka/reviz/MainActivity.java
+++ app/src/main/java/ru/kosjka/reviz/MainActivity.java
@@ -1,8 +1,11 @@
 package ru.kosjka.reviz;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,15 +22,27 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnScan,btnOk,btnTest;
+    private Button btnScan, btnOk, btnOpen, btnSend;
     public TextView tvBarcode, tvFullName, tvQty;
     private DBHelper dbHelper;
     private EditText etQty;
     private String ngoods;
     private final String MYLOG = "MainActivity";
     final Activity activity = this;
+    private final int REQUEST_SCAN = 1;
+    private final int REQUEST_DBSELECT = 2;
+    private final int REQUEST_SEND = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +51,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dbHelper = new DBHelper(this);
         btnScan = (Button) this.findViewById(R.id.btnScan);
         btnOk = (Button) this.findViewById(R.id.btnOk);
-        btnTest = (Button) this.findViewById(R.id.btnTest);
-        btnOk.setOnClickListener( this);
-        btnTest.setOnClickListener( this);
+        btnOpen = (Button) this.findViewById(R.id.btnOpen);
+        btnSend = (Button) this.findViewById(R.id.btnSend);
 
+        btnOk.setOnClickListener(this);
+        btnOpen.setOnClickListener(this);
+        btnSend.setOnClickListener(this);
 
-        tvBarcode =(TextView) this.findViewById(R.id.tvBarcode);
-        tvFullName =(TextView) this.findViewById(R.id.tvFullName);
-        tvQty =(TextView) this.findViewById(R.id.tvQty);
+        tvBarcode = (TextView) this.findViewById(R.id.tvBarcode);
+        tvFullName = (TextView) this.findViewById(R.id.tvFullName);
+        tvQty = (TextView) this.findViewById(R.id.tvQty);
 
         etQty = (EditText) this.findViewById(R.id.etQty);
         //Log.d(MYLOG,"-----"+tvBarcode.getText().toString() );
 
+        dbHelper = new DBHelper(this);
+
+        //Check exists database
+        File database = getApplicationContext().getDatabasePath(DBHelper.DBNAME);
+        if (!database.exists()) {
+            dbHelper.getReadableDatabase();
+            //Copy db
+            if (copyDatabase(this)) {
+                Toast.makeText(this, "Copy database succes", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Copy data error", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
 
 
         btnScan.setOnClickListener(new View.OnClickListener() {
@@ -59,18 +90,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 integrator.setCameraId(0);
                 integrator.setBeepEnabled(false);
                 integrator.setBarcodeImageEnabled(false);
+                integrator.setOrientationLocked(false);
                 integrator.initiateScan();
             }
         });
 
     }
 
+    private boolean copyDatabase(Context context) {
+        try {
+
+            InputStream inputStream = context.getAssets().open(DBHelper.DBNAME);
+            String outFileName = DBHelper.DBLOCATION + DBHelper.DBNAME;
+            OutputStream outputStream = new FileOutputStream(outFileName);
+            byte[] buff = new byte[1024];
+            int length = 0;
+            while ((length = inputStream.read(buff)) > 0) {
+                outputStream.write(buff, 0, length);
+            }
+            outputStream.flush();
+            outputStream.close();
+            Log.w("MainActivity", "DB copied");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
+
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
+        if (result != null) {
+            if (result.getContents() == null) {
                 Log.d(MYLOG, "Cancelled scan");
 
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
@@ -78,20 +132,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d(MYLOG, "Scanned");
                 tvBarcode.setText(result.getContents());
                 //Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
-                if(tvBarcode.getText().toString() != "") findGoods();
+                if (tvBarcode.getText().length() != 0) findGoods();
             }
         } else {
-            // This is important, otherwise the result will not be passed to the fragment
             super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK) {
+                switch (requestCode) {
+//                                case REQUEST_SCAN:
+//                                    break;
+                    case REQUEST_DBSELECT:
+                        try {
+                            //File file = new File("/sdcard/Download/reviz.db");
+                            //InputStream inputStream = new FileInputStream(file);
+                            ContentResolver cr = getContentResolver();
+                            InputStream inputStream = cr.openInputStream(data.getData());
+                            String outFileName = DBHelper.DBLOCATION + DBHelper.DBNAME;
+                            OutputStream outputStream = new FileOutputStream(outFileName);
+                            byte[] buff = new byte[1024];
+                            int length = 0;
+                            while ((length = inputStream.read(buff)) > 0) {
+                                outputStream.write(buff, 0, length);
+                            }
+                            outputStream.flush();
+                            outputStream.close();
+                            Log.w("MainActivity", "DB copied");
+                            Toast.makeText(this, "Ура", Toast.LENGTH_LONG).show();//return true;
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            ///return false;
+                        }
+
+                        break;
+
+                }
+            }
+
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString("tvBarcode",tvBarcode.getText().toString());
-        outState.putString("tvFullName",tvFullName.getText().toString());
-        outState.putString("tvQty",tvQty.getText().toString());
+        outState.putString("tvBarcode", tvBarcode.getText().toString());
+        outState.putString("tvFullName", tvFullName.getText().toString());
+        outState.putString("tvQty", tvQty.getText().toString());
+        outState.putString("ngoods", ngoods);
         Log.d(MYLOG, "saved");
     }
 
@@ -101,6 +186,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvBarcode.setText(savedInstanceState.getString("tvBarcode"));
         tvFullName.setText(savedInstanceState.getString("tvFullName"));
         tvQty.setText(savedInstanceState.getString("tvQty"));
+        this.ngoods = savedInstanceState.getString("ngoods");
         Log.d(MYLOG, "restore");
 
     }
@@ -110,23 +196,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         SQLiteDatabase database = dbHelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
 
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btnOk:
-                   if (etQty.getText().toString().trim().length()!=0){
-                       Log.d(MYLOG,"--"+etQty.getText().toString()+"--");
-                       contentValues.put("qty",Integer.parseInt(etQty.getText().toString()));
-                       database.update("goods",contentValues,"ngoods = ?",new String[] {ngoods});
-                       findGoods();
-                       etQty.setText("");
-                   }
+                if (etQty.getText().toString().trim().length() != 0) {
+                    Log.d(MYLOG, "--" + etQty.getText().toString() + "--");
+                    contentValues.put("qty", Integer.parseInt(etQty.getText().toString()));
+                    database.update("goods", contentValues, "ngoods = ?", new String[]{ngoods});
+                    findGoods();
+                    etQty.setText("");
+                }
                 break;
-            case R.id.btnTest:
-                Cursor cursor = database.rawQuery("select * from goods",null);
-                logCursor(cursor);
-                cursor.close();
+            case R.id.btnOpen:
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("application/octet-stream");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, REQUEST_DBSELECT);
+                break;
+            case R.id.btnSend:
+                if (copy(DBHelper.DBLOCATION + DBHelper.DBNAME, "/sdcard/send.db")) {
+                    File filelocation = new File("/sdcard/send.db");
+                    Uri path = Uri.fromFile(filelocation);
+                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+// set the type to 'email'
+                    emailIntent.setType("*/*");
+                    String to[] = {"kosjka@ro.ru"};
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+// the attachment
+                    emailIntent.putExtra(Intent.EXTRA_STREAM, path);
+// the mail subject
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+                    startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                }
                 break;
         }
     }
+
     void logCursor(Cursor cursor) {
         if (cursor != null) {
             if (cursor.moveToFirst()) {
@@ -141,15 +245,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         } else Log.d(MYLOG, "Cursor is null");
     }
-    void findGoods(){
+
+    void findGoods() {
         SQLiteDatabase database = dbHelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         Log.d(MYLOG, "start find ngoods");
         Cursor cursor = database.rawQuery("select g.ngoods, g.fullname, g.qty from barcodes b " +
                         "inner join goods g on b.ngoods = g.ngoods where b.barcode = ?",
-                new String[] {tvBarcode.getText().toString()});
+                new String[]{tvBarcode.getText().toString()});
         Log.d(MYLOG, "finish find ngoods");
-        if(cursor != null && cursor.getCount() == 1 ) {
+        if (cursor != null && cursor.getCount() == 1) {
             cursor.moveToFirst();
             Log.d(MYLOG, "cursor out start");
             tvFullName.setText(cursor.getString(cursor.getColumnIndex("fullname")));
@@ -159,14 +264,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ngoods = cursor.getString(cursor.getColumnIndex("ngoods"));
             Log.d(MYLOG, "2");
 
-        }else{
-            //Toast.makeText(this, "Не найдено!!!", Toast.LENGTH_LONG).show();
+        } else {
             tvFullName.setText("Товар не найден!");
             tvQty.setText("0");
         }
         cursor.close();
     }
-    void updateQty(){
+
+    void updateQty() {
 
     }
+
+    public static boolean copy(String from, String to) {
+        try {
+            File file = new File(from);
+            InputStream inputStream = new FileInputStream(file);
+//            ContentResolver cr = getContentResolver();
+//            InputStream inputStream = cr.openInputStream(data.getData());
+            String outFileName = to;
+            OutputStream outputStream = new FileOutputStream(outFileName);
+            byte[] buff = new byte[1024];
+            int length = 0;
+            while ((length = inputStream.read(buff)) > 0) {
+                outputStream.write(buff, 0, length);
+            }
+            outputStream.flush();
+            outputStream.close();
+            Log.w("MainActivity", "DB copied");
+            //Toast.makeText(this, "Ура", Toast.LENGTH_LONG).show();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+
 }
